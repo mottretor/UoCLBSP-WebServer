3a0ad2d3367cf6df208f827a136f571a02bb945f
PasinduPriyashan <pasindu95.m@gmail.com>
2017-12-27 10:54:39 +0530

    Added add_roads view

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Welcome to UoC Maps</title>
 
 	<style type="text/css">
 
