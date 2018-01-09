9a71e143582b530784c529352316305437177da7
PasinduPriyashan <pasindu95.m@gmail.com>
2018-01-09 18:07:48 +0530

    Layout Optimizations

--- assets/js/admin_panel.js
+++ assets/js/admin_panel.js
@@ -1,22 +1,65 @@
 (function ($) {
-    $(function () { // DOM Ready
+                $(function () { // DOM Ready
 
-        // Toggle navigation
-        $('#nav-toggle').click(function () {
-            this.classList.toggle("active");
-            // If sidebar is visible:
-            if ($('body').hasClass('show-nav')) {
-                // Hide sidebar
-                $('body').removeClass('show-nav');
-            } else { // If sidebar is hidden:
-                $('body').addClass('show-nav');
-                // Display sidebar
-            }
-        });
-    });
-})(jQuery);
+                    // Toggle navigation
+                    $('#nav-toggle').click(function () {
+                        // console.log( $('#nav-toggle').classList);
+                        // console.log(this);
+                        this.classList.toggle("active");
+                        // If sidebar is visible:
+                        if ($('body').hasClass('show-nav')) {
+                            // Hide sidebar
+                            $('#cont').css('padding-right', '30px');
+                            $('body').removeClass('show-nav');
 
-$('building').click(function(){//element to be click to load the page in the div
-    $('cont').load('Manage_building/add');
+                        } else { // If sidebar is hidden:
+                            $('body').addClass('show-nav');
+                            $('#cont').css('padding-right', '280px');
+                            // Display sidebar
+                        }
+                    });
+                });
+            })(jQuery);
 
-});
+            $(document).ready(function () {
+                var trigger = $('#nav ul li a'),
+                        contain = $('#cont');
+
+                trigger.on('click', function () {
+                    var $this = $(this),
+                            target = $this.attr('data-target');
+                    // console.log(target+'.php');
+
+                    $.ajax({
+                        url:  target,
+                        method: 'GET',
+                        success: function (data) {
+                            contain.html(data);
+
+
+                            // Toggle navigation
+                            // $('#nav-toggle').click(function () {
+                            // console.log( $('#nav-toggle').classList);
+                            // $('#nav-toggle').classList.toggle("active");
+                            document.getElementById('nav-toggle').classList.toggle("active");
+                            // If sidebar is visible:
+                            if ($('body').hasClass('show-nav')) {
+                                // Hide sidebar
+                                $('body').removeClass('show-nav');
+                            } else { // If sidebar is hidden:
+                                $('body').addClass('show-nav');
+
+                                // Display sidebar
+                            }
+
+                            return false;
+                            // });
+                        }
+                    });
+
+                    //contain.load('<?php //echo base_url();  ?>//'+ 'index.php/Manage_building/add_building');
+
+                    // return false;
+
+                });
+            });
