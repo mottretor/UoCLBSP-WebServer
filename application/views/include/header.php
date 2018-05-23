89a80db3bbd88ce96dd2df6b399b5d0c0f0a7c71
Udith Jayasundara <jayasundara.udith@gmail.com>
2018-05-23 18:04:48 +0530

    Revert "Add Google Sign for websites"
    This reverts commit e7ef77d57d785ab38791b9aaaa348ed1591795c8.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -3,7 +3,6 @@
 
 <head>
     <title>UoC Maps</title>
-    <meta name="google-signin-client_id" content="<?=$this->config->item('api_key');?>.apps.googleusercontent.com">
     <link href="<?php echo base_url(); ?>/assets/drawable/favicon.ico" rel="shortcut icon" type="image/x-icon" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/admin_styles.css" >
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/fontawesome-all.css" >
@@ -18,7 +17,6 @@
     </script>';
     }
     ?>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
 <body>
 
@@ -27,11 +25,8 @@
 
 <header>
            <div class="container">
-                   <div>
-                       <a style="float: left;"data-target="Admin_home/search"><img src="<?php echo base_url();?>/assets/drawable/web_icon.png" height="45px"></a>
-                   </div>
-                   <div style="float: left;" class="g-signin2" data-onsuccess="onSignIn"></div>
-                   </div>
+          <a data-target="Admin_home/search"><img src="<?php echo base_url();?>/assets/drawable/web_icon.png" height="45px"></a>
+         </div>
 </header>
 
 
