af0bd24d780364963c42c90762160bd437df6fc1
Udith Jayasundara <jayasundara.udith@gmail.com>
2018-05-24 21:47:57 +0530

    Add google sign-in for websites

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -7,6 +7,23 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/admin_styles.css" >
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/fontawesome-all.css" >
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+    <meta name="google-signin-client_id" content="709099748867-9u7gorvp68vjcban1acqrhfd9ofo1fqg.apps.googleusercontent.com">
+
+    <script>
+        function onSignIn(googleUser) {
+            /*
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            */
+            window.location.href = "<?php echo site_url();?>" ; //redirect controller goes inside site_url() function.
+        }
+
+    </script>
 <!--    <script type = 'text/javascript' src = "--><?php //echo base_url(); ?><!--/assets/js/admin_panel.js"></script>-->
     <?php if($call == True) {
         echo '<script>
@@ -27,6 +44,7 @@
            <div class="container">
           <a data-target="Admin_home/search"><img src="<?php echo base_url();?>/assets/drawable/web_icon.png" height="45px"></a>
          </div>
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
 </header>
 
 
